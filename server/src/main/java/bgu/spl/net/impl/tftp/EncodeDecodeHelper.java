b18910e9655fa47ad1bf28acf2a4268c56156b5c
aaroniz <aaroni@post.bgu.ac.il>
2024-02-29 13:53:54 +0200

    Util functions to help with encdec shorts and bytes

---
+++ server/src/main/java/bgu/spl/net/impl/tftp/EncodeDecodeHelper.java
@@ -0,0 +1,29 @@
+package bgu.spl.net.impl.tftp;
+
+/**
+ * Uses the big endian form.
+ */
+public class EncodeDecodeHelper {
+
+    /**
+     * Converts a short to a byte array.
+     * @param convert a short to convert.
+     * @return a byte array containing the short data.
+     */
+    public static byte[] shortToByte(short convert) {
+        return new byte[]{(byte) (convert >> 8), (byte) (convert & 0xff)};
+    }
+
+    /**
+     * Converts a byte array to a short.
+     * @param bytes 2 length byte array.
+     * @return a short equal to the input.
+     * @throws IllegalArgumentException if the byte array does not contain exactly 2 entries.
+     */
+    public static short byteToShort(byte[] bytes) {
+        if(bytes.length != 2) {
+            throw new IllegalArgumentException("Cannot convert into a short, contains more/less than 2 bytes.");
+        }
+        return (short) (((short) bytes[0] << 8 | (short) (bytes[1])));
+    }
+}

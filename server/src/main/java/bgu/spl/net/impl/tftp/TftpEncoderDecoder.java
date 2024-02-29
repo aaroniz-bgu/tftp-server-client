19da6640d0e1f866bb82fbfd6296d10a520f1a79
aaroniz <aaroni@post.bgu.ac.il>
2024-02-29 19:34:58 +0200

    fixed and completed encdec

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -2,20 +2,65 @@ package bgu.spl.net.impl.tftp;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
 
-public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
-    //TODO: Implement here the TFTP encoder and decoder
+import java.util.LinkedList;
 
+import static bgu.spl.net.impl.tftp.GlobalConstants.TERMINATOR;
+import static bgu.spl.net.impl.tftp.Operation.*;
+
+public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
+    private LinkedList<Byte> message;
+    private Operation operation;
+    private boolean isData;
+    private int packetSize;
+
+    public TftpEncoderDecoder() {
+        message = new LinkedList<>();
+        operation = NO_OP;
+        packetSize = -1;
+        isData = false;
+    }
 
     @Override
     public byte[] decodeNextByte(byte nextByte) {
-        // TODO: implement this
-
+        message.add(nextByte);
+        // Determine op code:
+        if(message.size() == 2) {
+            operation = Operation.OPS[EncodeDecodeHelper.byteToShort(new byte[]{message.getFirst(), message.getLast()})];
+            // determine length to read if not terminated by delimiter.
+            if(operation == DIRQ || operation == DISC) {
+                // those are ending after 2 bytes.
+                packetSize = 2;
+            } else if(operation == DATA) {
+                // if that's a data packet, wait until the 4'th byte was read.
+                isData = true;
+            } else if (operation == ACK) {
+                packetSize = 4;
+            }
+        }
+        if(isData && message.size() == 4) {
+            packetSize = EncodeDecodeHelper.byteToShort(new byte[]{message.get(2), message.getLast()}) + 6;
+        }
+        if(messageComplete()) {
+            byte[] result = new byte[message.size()];
+            int i = 0;
+            for(byte b : message) {
+                result[i] = b;
+                i++;
+            }
+            message = new LinkedList<>();
+            operation = NO_OP;
+            packetSize = -1;
+            return result;
+        }
         return null;
+    }
+
+    public boolean messageComplete() {
+        return (operation.TERMINATED && message.getLast().equals(TERMINATOR)) || (message.size() == packetSize);
     }
 
     @Override
     public byte[] encode(byte[] message) {
-        //TODO: implement this
-        return null;
+        return message;
     }
 }

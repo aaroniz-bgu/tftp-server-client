0e71fd466e33a0842ed9384788f6273064e5c7c4
aaroniz <aaroni@post.bgu.ac.il>
2024-03-02 19:32:40 +0200

    added observer pattern

--- server/src/main/java/bgu/spl/net/srv/Connections.java
+++ server/src/main/java/bgu/spl/net/srv/Connections.java
@@ -9,4 +9,18 @@ public interface Connections<T> {
     boolean send(int connectionId, T msg);
 
     void disconnect(int connectionId);
+
+    // Added observer design pattern for broadcasting
+
+    /**
+     * Subscribes to the listeners.
+     * @param connectionId The connection id to be subscribed.
+     */
+    void subscribe(int connectionId);
+
+    /**
+     * Usually called notify(), to broadcast messages easier.
+     * @param broadcastMessage Message to send.
+     */
+    void broadcast(T broadcastMessage);
 }

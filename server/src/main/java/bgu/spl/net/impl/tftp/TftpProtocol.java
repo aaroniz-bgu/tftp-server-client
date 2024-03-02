3a7ac7966f692731481912377ab40c8c72403b33
aaroniz <aaroni@post.bgu.ac.il>
2024-03-02 20:23:01 +0200

    Added login to prevent user from creating requests if it's not logged in and to not login twice from different machines

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
@@ -5,6 +5,8 @@ import bgu.spl.net.impl.tftp.controllers.TftpApi;
 import bgu.spl.net.impl.tftp.packets.AbstractPacket;
 import bgu.spl.net.srv.Connections;
 
+import java.util.NoSuchElementException;
+
 public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
 
     private TftpApi controller;
@@ -13,6 +15,7 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
     private Connections<byte[]> connections;
 
     private boolean terminate = false;
+    private boolean isLogged = false;
 
     @Override
     public void start(int connectionId, Connections<byte[]> connections) {
@@ -29,10 +32,23 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
         if(message.length < 2) {
             throw new RuntimeException("Message received without operation code");
         }
+
+        // Retrieve op code:
         short opCode = EncodeDecodeHelper.byteToShort(new byte[]{message[0], message[1]});
         Operation op = Operation.OPS[opCode];
+
+        // Check if we're logged before giving any service:
+        if(!isLogged) {
+            if(op == Operation.LOGRQ) {
+                login("TODO"); // TODO Retrieve username from message.
+            }
+            return;
+        }
+
+        // Call the API and get response:
         AbstractPacket controllerResponse = mapMessageToApi(controller, op, message);
 
+        // Handle response:
         if(controllerResponse != null) {
             AbstractPacket broadcast = controllerResponse.getBroadcastPacket();
             if(broadcast != null) {
@@ -59,6 +75,31 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
         }
     }
 
+    /**
+     * Logs in connection so it could use the server.
+     * @param username The username the connections wants to use.
+     */
+    private void login(String username) {
+        try {
+            connections.subscribe(connectionId, username);
+            connections.send(connectionId, null /*TODO new AcknowledgementPacket(0).getBytes()*/);
+            isLogged = true;
+        } catch (SecurityException e) {
+            //we need a logger.
+            connections.send(connectionId, null /*TODO new ErrorPacket(e.getMessage()).getBytes()*/);
+            isLogged = false;
+        } catch (NoSuchElementException e) {
+            //log that this connection doesn't exist.
+        }
+    }
+
+    /**
+     * Maps requests to the appropriate end-point.
+     * @param controller The controller that this user uses.
+     * @param opCode The operation/service user is requesting.
+     * @param request The request contents.
+     * @return The APIs response.
+     */
     private static AbstractPacket mapMessageToApi(TftpApi controller, Operation opCode, byte[] request) {
         switch(opCode) {
             case RRQ:
@@ -70,8 +111,11 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
                 return null;
             case DIRQ:
                 return controller.listDirectoryRequest();
+            /*
+            Unnecessary, now using TftpProtocol.login() for this.
             case LOGRQ:
                 return controller.logRequest(request);
+            */
             case DELRQ:
                 return controller.deleteRequest(request);
             case DISC:

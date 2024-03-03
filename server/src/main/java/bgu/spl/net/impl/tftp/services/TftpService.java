7764ba9b1dbbaca619e23dcab3e5aa30ac0054f7
Daniel Sachs <sachsd@post.bgu.ac.il>
2024-03-03 20:20:22 +0200

    Initial implementation

--- server/src/main/java/bgu/spl/net/impl/tftp/services/TftpService.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/services/TftpService.java
@@ -1,4 +1,30 @@
 package bgu.spl.net.impl.tftp.services;
 
+import bgu.spl.net.impl.tftp.packets.AbstractPacket;
+
 public class TftpService implements ITftpService {
+    @Override
+    public AbstractPacket deleteFile(String filename) {
+        return null;
+    }
+
+    @Override
+    public byte[] readRequest(String filename) throws IllegalArgumentException {
+        return new byte[0];
+    }
+
+    @Override
+    public AbstractPacket writeRequest(String filename) {
+        return null;
+    }
+
+    @Override
+    public short writeData(byte[] data) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public byte[] directoryRequest() throws Exception {
+        return new byte[0];
+    }
 }

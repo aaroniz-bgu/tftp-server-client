142556c437ca9f3dda641e815e3ad051ddb47ac6
Daniel Sachs <sachsd@post.bgu.ac.il>
2024-03-05 13:48:59 +0200

    Started creating input parser thread and request queue

--- client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
@@ -1,7 +1,32 @@
 package bgu.spl.net.impl.tftp;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class TftpClient {
-    //TODO: implement the main logic of the client, when using a thread per client the main logic goes here
     public static void main(String[] args) {
-        System.out.println("implement me!");
+        if (args.length < 2) {
+            System.out.println("you must supply two arguments: host, port");
+            System.exit(1);
+        }
+
+        ConcurrentLinkedQueue<String> requestQueue = new ConcurrentLinkedQueue<>();
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        // TODO wait somewhere
+        Thread inputManager = new Thread(() -> {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+                while (!Thread.currentThread().isInterrupted()) {
+                    String line = in.readLine();
+                    if (line != null) {
+                        requestQueue.add(line);
+                    }
+                }
+            } catch (IOException ex) {
+            }
+        });
     }
 }

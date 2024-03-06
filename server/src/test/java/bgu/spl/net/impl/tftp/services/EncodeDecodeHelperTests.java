6f62146f08faa479cb5265a27c02abc227219f00
aaroniz <aaroni@post.bgu.ac.il>
2024-03-06 22:25:40 +0200

    -

--- server/src/test/java/bgu/spl/net/impl/tftp/services/EncodeDecodeHelperTests.java
+++ server/src/test/java/bgu/spl/net/impl/tftp/services/EncodeDecodeHelperTests.java
@@ -8,12 +8,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EncodeDecodeHelperTests {
 
-    @Test
-    void test() {
-        for(byte i = 0; i < Byte.MAX_VALUE; i++)
-            for(byte j = 0; j < Byte.MAX_VALUE; j++) {
-                short actual = EncodeDecodeHelper.byteToShort(new byte[]{j,i});
-                assertEquals(((short) j) * 128 + ((short) i), actual);
-            }
-    }
 }

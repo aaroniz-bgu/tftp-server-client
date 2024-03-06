371a6009a6d4c74d9ce552eb47c4b3ea61decce6
aaroniz <aaroni@post.bgu.ac.il>
2024-03-06 22:47:57 +0200

    buffer reader and logs

--- client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpClient.java
@@ -4,7 +4,8 @@ import bgu.spl.net.impl.tftp.packets.AbstractPacket;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.concurrent.SynchronousQueue;
+
+import static bgu.spl.net.impl.tftp.DisplayMessage.print;
 
 public class TftpClient {
     public static void main(String[] args) {
@@ -16,30 +17,41 @@ public class TftpClient {
         String host = args[0];
         int port = Integer.parseInt(args[1]);
 
+        print("Starting client...");
+        print("Initiating resources...");
+
         TftpEncoderDecoder encdec = new TftpEncoderDecoder();
         TftpProtocol protocol = new TftpProtocol();
         CliInterface inter = new CliInterface(protocol.getCoordinator());
         Thread interfaceThread = new Thread(inter, "Interface-Thread");
 
-        try (Socket sock = new Socket(host, port)) {
+        print("Attempting to connect to server");
+
+        try (Socket sock = new Socket(host, port);
+             BufferedInputStream in = new BufferedInputStream(sock.getInputStream());
+             BufferedOutputStream out = new BufferedOutputStream(sock.getOutputStream())
+        ) {
+
+            print("Starting client...");
 
             interfaceThread.start();
+
+            print("Client is ready!");
 
             while (!Thread.currentThread().isInterrupted()) {
                 // Check if the CLI made a request:
                 AbstractPacket send = protocol.getCoordinator().getRequest();
                 if(send != null) {
-                    sock.getOutputStream().write(encdec.encode(send.getBytes()));
+                    out.write(encdec.encode(send.getBytes()));
                 }
-
                 // If the server has sent us a message:
-                byte[] msg = encdec.decodeNextByte((byte) sock.getInputStream().read());
+                byte[] msg = encdec.decodeNextByte((byte) in.read());
                 if(msg != null) {
                     // Process the server's message:
                     byte[] response = protocol.process(msg);
                     // If the server waits for our response:
                     if(response != null) {
-                        sock.getOutputStream().write(encdec.encode(response));
+                        out.write(encdec.encode(response));
                     }
                 }
             }

ecb1a50081c1c6cb0b0db6fb10f862a058cdbdca
aaroniz <aaroni@post.bgu.ac.il>
2024-03-08 18:14:50 +0200

    for heddy

--- server/src/main/java/bgu/spl/net/TftpStartup.java
+++ server/src/main/java/bgu/spl/net/TftpStartup.java
@@ -10,20 +10,5 @@ import java.util.MissingResourceException;
 
 public class TftpStartup {
 
-    public static void main(String[] args) {
-        if(args.length == 0) {
-            throw new MissingFormatArgumentException("Missing port.");
-        }
-        int port = Integer.parseInt(args[0]);
-        try {
-            System.out.println("Starting server...");
-            BaseServer<byte[]> server = new TftpServer(port);
-            server.serve();
-            server.close();
-        } catch(Exception e) {
-            System.out.println("Error occurred while running the server:\n");
-            e.printStackTrace();
-        }
-        System.out.println("Server was forced to shut down.");
-    }
+    // Main class used to be here but BGU-SPL don't want it here :'(....
 }
--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -8,6 +8,7 @@ import bgu.spl.net.srv.BlockingConnectionHandler;
 import bgu.spl.net.srv.Connections;
 
 import javax.swing.*;
+import java.util.MissingFormatArgumentException;
 import java.util.function.Supplier;
 
 /**
@@ -15,6 +16,23 @@ import java.util.function.Supplier;
  * This server handles messages by serializing packets into byte streams.
  */
 public class TftpServer extends BaseServer<byte[]> {
+
+    public static void main(String[] args) {
+        if(args.length == 0) {
+            throw new MissingFormatArgumentException("Missing port.");
+        }
+        int port = Integer.parseInt(args[0]);
+        try {
+            System.out.println("Starting server...");
+            BaseServer<byte[]> server = new TftpServer(port);
+            server.serve();
+            server.close();
+        } catch(Exception e) {
+            System.out.println("Error occurred while running the server:\n");
+            e.printStackTrace();
+        }
+        System.out.println("Server was forced to shut down.");
+    }
 
     public TftpServer(int port) {
         super(port, TftpProtocol::new, TftpEncoderDecoder::new, new TftpConnections());
